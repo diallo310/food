9808fce813eedf3952eab9875f101540be941da3
Hawaou diallo <diallohawaou310@gmail.com>
2021-04-28 15:58:23 +0200

    Category  finished

--- src/main/java/com/gn/food/dao/models/Category.java
+++ src/main/java/com/gn/food/dao/models/Category.java
@@ -1,15 +1,38 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.gn.food.dao.models;
 
 public class Category {
     private Integer categoryId;
     private String name;
     private String variant;
-=======
-package com.gn.food.dao.models;public class Category {
->>>>>>> parent of be42c44... Create category
-=======
-package com.gn.food.dao.models;public class Category {
->>>>>>> parent of be42c44... Create category
+
+
+    public Category(Integer categoryId, String name, String variant) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.variant = variant;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVariant() {
+        return variant;
+    }
+
+    public void setVariant(String variant) {
+        this.variant = variant;
+    }
 }
