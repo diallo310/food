b842427eb39cd29f57b0658028b04c7a293ae4be
Hawaou diallo <diallohawaou310@gmail.com>
2021-04-28 15:55:49 +0200

    Category  finished

--- src/main/java/com/gn/food/dao/models/Category.java
+++ src/main/java/com/gn/food/dao/models/Category.java
@@ -1,10 +1,14 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package com.gn.food.dao.models;
 
 public class Category {
     private Integer categoryId;
     private String name;
     private String variant;
+=======
+package com.gn.food.dao.models;public class Category {
+>>>>>>> parent of be42c44... Create category
 =======
 package com.gn.food.dao.models;public class Category {
 >>>>>>> parent of be42c44... Create category
