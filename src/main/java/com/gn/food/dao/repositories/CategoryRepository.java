f69156c03030fc2ea0a4d33a61519810920f5b7a
Hawaou diallo <diallohawaou310@gmail.com>
2021-04-30 13:45:00 +0200

    Create CategoryRepository

--- src/main/java/com/gn/food/dao/repositories/CategoryRepository.java
+++ src/main/java/com/gn/food/dao/repositories/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.gn.food.dao.repositories;
 import com.gn.food.dao.models.Category;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CategoryProduct extends CrudRepository<Category,Integer> {
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
 }
