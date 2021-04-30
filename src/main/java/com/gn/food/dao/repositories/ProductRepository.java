137356bdc08fcd20b5c547833ffc34f10543c4a8
Hawaou diallo <diallohawaou310@gmail.com>
2021-04-30 13:42:42 +0200

    Create ProductRepository

--- src/main/java/com/gn/food/dao/repositories/ProductRepository.java
+++ src/main/java/com/gn/food/dao/repositories/ProductRepository.java
@@ -3,5 +3,5 @@ package com.gn.food.dao.repositories;
 import com.gn.food.dao.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product,Integer> {
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 }
