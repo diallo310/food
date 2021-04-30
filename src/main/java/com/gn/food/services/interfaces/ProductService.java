ae16c4c1258d45f8cc519c9c3013f37b6a5a891e
Hawaou diallo <diallohawaou310@gmail.com>
2021-04-30 13:41:03 +0200

    Add inteface ProductService

--- src/main/java/com/gn/food/services/interfaces/ProductService.java
+++ src/main/java/com/gn/food/services/interfaces/ProductService.java
@@ -1,2 +1,23 @@
-package com.gn.food.services.interfaces;public interface ProductService {
+package com.gn.food.services.interfaces;
+
+import com.gn.food.services.requests.ProductRequest;
+import com.gn.food.services.responses.ProductItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface ProductService {
+    void create(ProductRequest productRequest);
+
+    Optional<ProductItem> findById(int productId);
+
+    Optional<ProductItem> update(ProductRequest productRequest);
+
+    Page<ProductItem> findAll(Pageable pageable);
+
+    void delete(int productId);
+
+    void findByPrice(double price);
+
 }
