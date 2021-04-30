942b240bebbb983f03c5d591e52cd02753a2e82c
Hawaou diallo <diallohawaou310@gmail.com>
2021-04-30 13:39:05 +0200

    add description to product

--- src/main/java/com/gn/food/dao/models/Product.java
+++ src/main/java/com/gn/food/dao/models/Product.java
@@ -1,34 +1,36 @@
 package com.gn.food.dao.models;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer productId;
+    private int productId;
     private String name;
-    private Double price;
-    private Integer quantity;
+    private double price;
+    private int quantity;
     private String picture;
+    private String description;
     @ManyToOne
     private Category category;
 
-    public Product(final String name, final Double price, final Integer quantity, final String picture, final Category category) {
-        this.productId = productId;
+    public Product(final String name, final double price, final int quantity, final String picture, final String description, final Category category) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.picture = picture;
+        this.description = description;
         this.category = category;
     }
 
-    public Integer getProductId() {
+    public int getProductId() {
         return productId;
     }
 
-    public void setProductId(Integer productId) {
+    public void setProductId(int productId) {
         this.productId = productId;
     }
 
@@ -40,19 +42,19 @@ public class Product {
         this.name = name;
     }
 
-    public Double getPrice() {
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public Integer getQuantity() {
+    public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(Integer quantity) {
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
@@ -64,12 +66,33 @@ public class Product {
         this.picture = picture;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public Category getCategory() {
         return category;
     }
 
     public void setCategory(Category category) {
         this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productId == product.productId && Double.compare(product.price, price) == 0 && quantity == product.quantity && Objects.equals(name, product.name) && Objects.equals(picture, product.picture) && Objects.equals(category, product.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, name, price, quantity, picture, category);
     }
 }
 
