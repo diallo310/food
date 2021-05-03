db80101af537481c1e4248abc534606b91963426
Hawaou diallo <diallohawaou310@gmail.com>
2021-05-03 17:26:01 +0200

    Find All categories in Controller

--- src/main/java/com/gn/food/webservice/controllers/CategoryController.java
+++ src/main/java/com/gn/food/webservice/controllers/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("categories")
@@ -30,9 +31,14 @@ public class CategoryController {
         return ResponseEntity.of(categoryService.findById(categoryId));
     }
 
+    @GetMapping
+    public List<CategoryItem> findAll() {
+        return categoryService.findAll();
+    }
+
     @PatchMapping("/{categoryId}")
-    public ResponseEntity<CategoryItem>update(@PathVariable("categoryId") int categoryId, @Valid @RequestBody CategoryRequestUpdate categoryRequestUpdate){
-        return ResponseEntity.of(categoryService.update(categoryId,categoryRequestUpdate));
+    public ResponseEntity<CategoryItem> update(@PathVariable("categoryId") int categoryId, @Valid @RequestBody CategoryRequestUpdate categoryRequestUpdate) {
+        return ResponseEntity.of(categoryService.update(categoryId, categoryRequestUpdate));
     }
 
 }
